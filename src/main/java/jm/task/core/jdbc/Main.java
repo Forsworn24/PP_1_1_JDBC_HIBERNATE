4bdeed5577f857bb901c8ea90da2a9cfb41c2a44
Forsworn24 <mudinov.ali2401@gmail.com>
2023-08-16 22:34:16 +0300

    initial commit to hibernate path branch

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,21 +7,6 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
-
-        userService.createUsersTable();
-
-        userService.saveUser("Ivan", "Ivanov", (byte) 18);
-        userService.saveUser("Petr", "Petrov", (byte) 19);
-        userService.saveUser("Sidor", "Sidorov", (byte) 20);
-        userService.saveUser("Gleb", "Glebov", (byte) 21);
-
-        for (User user : userService.getAllUsers()) {
-            System.out.println(user.toString());
-        }
-
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
 
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,109 +8,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private final Connection connection = Util.getConnection();
 
     public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-        String queryCreateTable = "CREATE TABLE IF NOT EXISTS USER" +
-                "(ID INTEGER PRIMARY KEY AUTO_INCREMENT," +
-                "NAME VARCHAR(40)," +
-                "LASTNAME VARCHAR(40)," +
-                "AGE INTEGER)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(queryCreateTable)) {
-            connection.setAutoCommit(false);
-            preparedStatement.executeUpdate();
-            connection.commit();
-        } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
+
     }
 
     public void dropUsersTable() {
-        String queryDropTable = "DROP TABLE IF EXISTS USER";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(queryDropTable)) {
-            connection.setAutoCommit(false);
-            preparedStatement.executeUpdate();
-            connection.commit();
-        } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
+
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String querySaveUser = "INSERT INTO USER (NAME, LASTNAME, AGE) VALUES(?, ?, ?)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(querySaveUser)) {
-            connection.setAutoCommit(false);
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-            preparedStatement.executeUpdate();
-            connection.commit();
-            System.out.printf("User с именем - %s добавлен в базу данных\n", name);
-        } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
+
     }
 
     public void removeUserById(long id) {
-        String queryDeleteUser = "DELETE FROM USER WHERE ID=?";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(queryDeleteUser)) {
-            connection.setAutoCommit(false);
-            preparedStatement.setLong(1, id);
-            preparedStatement.executeUpdate();
-            connection.commit();
-        } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
     }
 
     public List<User> getAllUsers() {
-        List<User> users = new ArrayList<>();
-        String queryGetAllUsers = "SELECT * FROM USER";
-        try (ResultSet usersSet = connection.createStatement().executeQuery(queryGetAllUsers)) {
-            while (usersSet.next()) {
-                User user = new User(usersSet.getString("NAME"),
-                                        usersSet.getString("LASTNAME"),
-                                        usersSet.getByte("AGE"));
-                user.setId(usersSet.getLong("ID"));
-                users.add(user);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException();
-        }
-        return users;
+        return null;
     }
 
     public void cleanUsersTable() {
-        String queryCleanUsersTable = "TRUNCATE TABLE USER";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(queryCleanUsersTable)) {
-            connection.setAutoCommit(false);
-            preparedStatement.executeUpdate();
-            connection.commit();
-        } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
+
     }
 }
